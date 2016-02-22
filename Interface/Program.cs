32e92d1d4875f7a73ea88e717a799f8d0d3f8948
SuperBonny <bonora.matteo@gmail.com>
2016-02-22 20:51:50 +0100

    Fix untracked files

---
+++ Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using Machine;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Interface
+{
+	class MainClass
+	{
+		static Machine.Machine m = new Machine.Machine (new List<char> { '0', '1', '1', '0', '0' });
+
+		public static void Main (string[] args)
+		{
+			m.SetProgram (new List<string> (File.ReadAllLines (Path.Combine (Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location)
+				, @"Examples/invert.tm"))));
+
+			m.Run ("0", 500);
+		}
+	}
+}
