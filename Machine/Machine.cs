083193149536f61a34a2feeefda01ae39b9918ff
SuperBonny <bonora.matteo@gmail.com>
2016-02-22 20:18:27 +0100

    Add initial runtime code

--- Machine/Machine.cs
+++ Machine/Machine.cs
@@ -8,11 +8,11 @@ namespace Machine
 	{
 		public List<char> Memory { get; set; }
 
-		public List<string> Program { get; set; }
+		public List<string[]> Program { get; set; }
 
-		string state { get; set; }
+		public string state { get; set; }
 
-		int index;
+		int index = 0;
 
 		#region Constructors
 
@@ -26,7 +26,7 @@ namespace Machine
 			Memory = memory;
 		}
 
-		public Machine (List<char> memory, List<string> program)
+		public Machine (List<char> memory, List<string[]> program)
 		{
 			Memory = memory;
 			Program = program;
@@ -34,6 +34,42 @@ namespace Machine
 
 		#endregion
 
+		public void Run (string state = "0", int delay = 0)
+		{
+			string[] command = GetCommand ();
+
+			if (command [1] == "*" || Memory [index] == Convert.ToChar (command [1])) {
+				if (command [2] != "*")
+					Memory [index] = Convert.ToChar (command [2]);
+				switch (command [3]) {
+				case "r":
+					index++;
+					break;
+				case "l":
+					index--;
+					break;
+				}
+			}
+		}
+
+		string[] GetCommand ()
+		{
+			List<string[]> cmds = new List<string[]> ();
+			List<string> inp = new List<string> ();
+
+			foreach (string[] cmd in Program) {
+				if (cmd [0] == state) {
+					cmds.Add (cmd);
+					inp.Add (cmd [1]);
+				}
+			}
+
+			try {
+				return cmds [inp.IndexOf (Memory [index].ToString ())];
+			} catch {
+				return cmds [inp.IndexOf ("*")];
+			}
+		}
 	}
 }
 
