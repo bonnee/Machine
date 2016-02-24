5d28656ed36115aeba5196e3135773a1cdd14bee
SuperBonny <bonora.matteo@gmail.com>
2016-02-24 10:49:39 +0100

    Fix memory leak

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -7,13 +7,13 @@ namespace Interface
 {
 	class MainClass
 	{
-        static Machine.Machine m = new Machine.Machine(new List<char> { '1', '0', '1', '0', '1', '1', '1' });
+        static Machine.Machine m = new Machine.Machine(new List<char> { '1', '1', '1', '1', '1', '1', '1', '1' });
 
 		public static void Main (string[] args)
 		{
             m.SetProgram(new List<string>(File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"Examples/bintodec.tm"))));
 
-			m.Run ("0", 10);
+			m.Run ("0", 0);
 
             Console.ReadLine();
 		}
--- Machine/Machine.cs
+++ Machine/Machine.cs
@@ -4,84 +4,88 @@ using System.Threading;
 
 namespace Machine
 {
-	public class Machine
-	{
+    public class Machine
+    {
         List<char> mem = new List<char>();
         public List<char> Memory { get { return mem; } set { mem = value; mem.Insert(0, '_'); mem.Add('_'); } }
 
-		private List<string[]> Program { get; set; }
+        private List<string[]> Program { get; set; }
 
-		public string state { get; set; }
+        public string state { get; set; }
 
-		public int index = 0;
+        public int index = 0;
 
-		#region Constructors
+        #region Constructors
 
-		public Machine ()
-		{
-			
-		}
+        public Machine()
+        {
 
-		public Machine (List<char> memory)
-		{
-			Memory = memory;
-		}
+        }
 
-		public Machine (List<char> memory, List<string[]> program)
-		{
-			Memory = memory;
-			Program = program;
-		}
+        public Machine(List<char> memory)
+        {
+            Memory = memory;
+        }
 
-		#endregion
+        public Machine(List<char> memory, List<string[]> program)
+        {
+            Memory = memory;
+            Program = program;
+        }
 
-		/// <summary>
-		/// Sets the content of the program to be executed
-		/// </summary>
-		/// <param name="program">Program.</param>
-		public void SetProgram (List<string> program)
-		{
-			Program = new List<string[]> ();
-			foreach (string line in program)
-				Program.Add (line.Split (' '));
-		}
+        #endregion
 
-		#region Runtime
+        /// <summary>
+        /// Sets the content of the program to be executed
+        /// </summary>
+        /// <param name="program">Program.</param>
+        public void SetProgram(List<string> program)
+        {
+            Program = new List<string[]>();
+            foreach (string line in program)
+                Program.Add(line.Split(' '));
+        }
 
-		public void Run (string s,int delay = 0)
-		{
+        #region Runtime
+
+        public void Run(string s, int delay = 0)
+        {
             state = s;
+            string[] command;
             Print();
-			while (state != "halt") {
+            while (state != "halt")
+            {
                 Print();
 
-				string[] command = GetCommand ();
+                command = GetCommand();
 
-				if (command [1] == "*" || Memory [index] == Convert.ToChar (command [1])) {
-					if (command [2] != "*")
-						mem [index] = Convert.ToChar (command [2]);
-					switch (command [3]) {
-					case "r":
-                            if (index == Memory.Count-1)
+                if (command[1] == "*" || Memory[index] == Convert.ToChar(command[1]))
+                {
+                    if (command[2] != "*")
+                        mem[index] = Convert.ToChar(command[2]);
+                    switch (command[3])
+                    {
+                        case "r":
+                            if (index == Memory.Count - 1)
                                 Memory.Add('_');
                             else
                                 index++;
                             break;
-					case "l":
+                        case "l":
                             if (index == 0)
                                 Memory.Insert(0, '_');
                             else
                                 index--;
-						break;
-					}
+                            break;
+                    }
 
-					state = command [4];
-				}
-
-				Thread.Sleep (delay);
-			}
+                    state = command[4];
+                }
+                if (delay > 0)
+                    Thread.Sleep(delay);
+            }
             Print();
-		}
+        }
 
 
         void Print()
@@ -96,29 +100,34 @@ namespace Machine
             Console.Write("^\nState: " + state);
         }
 
-		/// <summary>
-		/// Gets the command to be executed between all the overloads of the current state
-		/// </summary>
-		/// <returns>The line.</returns>
-		string[] GetCommand ()
-		{
-			List<string[]> cmds = new List<string[]> ();
-			List<string> inp = new List<string> ();
+        /// <summary>
+        /// Gets the command to be executed between all the overloads of the current state
+        /// </summary>
+        /// <returns>The line.</returns>
+        string[] GetCommand()
+        {
+            List<string[]> cmds = new List<string[]>();
+            List<string> inp = new List<string>();
 
-			foreach (string[] cmd in Program) {
-				if (cmd [0] == state) {
-					cmds.Add (cmd);
-					inp.Add (cmd [1]);
-				}
-			}
+            foreach (string[] cmd in Program)
+            {
+                if (cmd[0] == state)
+                {
+                    cmds.Add(cmd);
+                    inp.Add(cmd[1]);
+                }
+            }
 
-			try {
+            try
+            {
                 return cmds[inp.IndexOf(mem[index].ToString())];
-            } catch {
+            }
+            catch
+            {
                 return cmds[inp.IndexOf("*")];
-			}
-		}
-	}
-	#endregion
+            }
+        }
+    }
+        #endregion
 }
 
